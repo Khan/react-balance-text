b31d2785d6bcb41fe5e2d44038a915958d0733f9
BrianGenisio <briangenisio@gmail.com>
2016-12-27 10:44:45 -0500

    Update the tests so it doesn't blow up, and comment that there are no tests at this time
    Test Plan:
    npm run test

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,10 +3,10 @@
 // import BalanceText from '../index';
 // import { expect } from 'chai';
 // import sinon from 'sinon';
-const {describe, it} = global;
+const {describe} = global;
 
 describe('BalanceText', () => {
-    it('is happy', () => {
-
-    });
+    // There are no unit tests right now.  This is a very hard
+    // component to unit test since it relies so muc on DOM manipulation.
+    // But this his here to keep the testing framework happy, for now.
 });
